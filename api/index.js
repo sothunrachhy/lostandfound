83686f4ce4d5104354a39828c6c70c86e1a7aad3
Sothun Rachhy <rachhy712@gmail.com>
2026-08-06 11:24:03 +0700

    Fix Vercel serverless URL routing in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
 const app = require('../server/server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  if (req.url && !req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? req.url : '/' + req.url);
+  }
+  return app(req, res);
+};
--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+// Vercel Serverless Function route prefix normalizer
+app.use((req, res, next) => {
+  if (req.url.startsWith('/api/') && req.url !== '/api') {
+    // Keep req.url untouched for Express routes matching /api/*
+  }
+  next();
+});
+
 app.get('/', (req, res) => {
   res.json({
     status: 'online',
